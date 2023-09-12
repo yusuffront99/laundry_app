4633e5f84f02f8ca0ac8669c7bb9ada9fb33b1d3
unknown <you@example.com>
2023-09-12 18:45:46 +0800

    updated codes -- dashboard_page

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:laundry_app/config/app.constant.dart';
+import 'package:laundry_app/providers/dashboard_provider.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
@@ -7,31 +9,41 @@ class DashboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AppConstants.navMenuDashboard[0]['view'] as Widget,
+      body: Consumer(builder: (_, wiRef, __) {
+        int navIndex = wiRef.watch(dashboardNavIndexProvider);
+        return AppConstants.navMenuDashboard[navIndex]['view'] as Widget;
+      }),
       extendBody: true, //===all color for bg
       // ==bottom menu navbar
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.fromLTRB(70, 0, 70, 20),
-        child: Material(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.white,
-          child: BottomNavigationBar(
-            elevation: 0,
-            backgroundColor: Colors.transparent,
-            iconSize: 30,
-            type: BottomNavigationBarType.fixed,
-            onTap: (value) {},
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-            unselectedItemColor: Colors.grey[400],
-            items: AppConstants.navMenuDashboard.map((e) {
-              return BottomNavigationBarItem(
-                icon: Icon(e['icon']),
-                label: e['label'],
-              );
-            }).toList(),
-          ),
-        ),
+        child: Consumer(builder: (_, wiRef, __) {
+          int navIndex = wiRef.watch(dashboardNavIndexProvider);
+          return Material(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.white,
+            elevation: 8,
+            child: BottomNavigationBar(
+              currentIndex: navIndex,
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              iconSize: 30,
+              type: BottomNavigationBarType.fixed,
+              onTap: (value) {
+                wiRef.read(dashboardNavIndexProvider.notifier).state = value;
+              },
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              unselectedItemColor: Colors.grey[400],
+              items: AppConstants.navMenuDashboard.map((e) {
+                return BottomNavigationBarItem(
+                  icon: Icon(e['icon']),
+                  label: e['label'],
+                );
+              }).toList(),
+            ),
+          );
+        }),
       ),
       // ==bottom menu navbar
     );
