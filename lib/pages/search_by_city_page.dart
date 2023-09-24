746ede3bdc02aa244e25fb597bf33f8ae30b1084
unknown <you@example.com>
2023-09-25 06:07:48 +0800

    added and updated ui search by city page

--- lib/pages/search_by_city_page.dart
+++ lib/pages/search_by_city_page.dart
@@ -1,42 +1,55 @@
+import 'package:d_view/d_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:laundry_app/config/app_colors.dart';
 import 'package:laundry_app/config/failure.dart';
 import 'package:laundry_app/datasources/shop_datasource.dart';
 import 'package:laundry_app/models/shop_model.dart';
+import 'package:laundry_app/providers/search_by_city_provider.dart';
 
-class SearchByCity extends StatefulWidget {
+class SearchByCity extends ConsumerStatefulWidget {
   const SearchByCity({super.key, required this.query});
   final String query;
 
   @override
-  State<SearchByCity> createState() => _SearchByCityState();
+  ConsumerState<SearchByCity> createState() => _SearchByCityState();
 }
 
-class _SearchByCityState extends State<SearchByCity> {
+class _SearchByCityState extends ConsumerState<SearchByCity> {
   final edtSearch = TextEditingController();
 
   execute() {
     ShopDataSource.searchByCity(edtSearch.text).then((value) {
+      setSearchByCityStatus(ref, 'Loading');
       value.fold(
         (failure) {
           switch (failure.runtimeType) {
             case ServerFailure:
+              setSearchByCityStatus(ref, 'Server Error');
               break;
             case NotFoundFailure:
+              setSearchByCityStatus(ref, 'Error Not Found');
               break;
             case ForbiddenFailure:
+              setSearchByCityStatus(ref, 'You don\'t have access');
               break;
             case BadRequestFailure:
+              setSearchByCityStatus(ref, 'Bad request');
               break;
             case UnauthorisedFailure:
+              setSearchByCityStatus(ref, 'Unauthorised');
               break;
             default:
+              setSearchByCityStatus(ref, 'Request Error');
               break;
           }
         },
         (result) {
+          setSearchByCityStatus(ref, 'success');
           List data = result['data'];
           List<ShopModel> list =
               data.map((e) => ShopModel.fromJson(e)).toList();
+          ref.read(searchByCityListProvider.notifier).setData(list);
         },
       );
     });
@@ -53,6 +66,86 @@ class _SearchByCityState extends State<SearchByCity> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        titleSpacing: 0,
+        title: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          height: 40,
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Row(
+            children: [
+              const Text(
+                'City: ',
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 16,
+                  height: 1,
+                ),
+              ),
+              Expanded(
+                child: TextField(
+                  controller: edtSearch,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    isDense: true,
+                  ),
+                  style: const TextStyle(height: 1),
+                  onSubmitted: (value) => execute(),
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () => execute(),
+            icon: const Icon(Icons.search),
+          ),
+        ],
+      ),
+
+      // ==== body search show
+      body: Consumer(
+        builder: (_, wiRef, __) {
+          String status = wiRef.watch(searchByCityStatusProvider);
+          List<ShopModel> list = wiRef.watch(searchByCityListProvider);
+
+          if (status == '') {
+            return DView.nothing();
+          }
+
+          if (status == 'Loading') {
+            return DView.loadingCircle();
+          }
+
+          if (status == 'success') {
+            return ListView.builder(
+              itemCount: list.length,
+              itemBuilder: (context, index) {
+                ShopModel shop = list[index];
+                return ListTile(
+                  onTap: () {},
+                  leading: CircleAvatar(
+                    backgroundColor: AppColors.primary,
+                    foregroundColor: Colors.white,
+                    radius: 18,
+                    child: Text('${index + 1}'),
+                  ),
+                  title: Text(shop.name),
+                  subtitle: Text(shop.city),
+                  trailing: const Icon(Icons.navigate_next),
+                );
+              },
+            );
+          }
+
+          return DView.error(status);
+        },
+      ),
+    );
   }
 }
